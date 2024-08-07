cb50279f8e0a1d986a2f20ba64d636ada5872044
Arthur <contato.arthurteruel@gmail.com>
2024-08-07 15:58:56 -0300

    Basic-Web-Theme-Switchet

---
+++ app.js
@@ -0,0 +1,12 @@
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    let className = document.body.className;
+    if (className.includes('light-theme')) {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+});
